297f5dedd67dadc0e8c8bea4454dbd546a8e4c2e
minhnhut23 <minhnhut23.work@gmail.com>
2025-04-14 13:27:31 +0700

    Constants

---
+++ constants.go
@@ -0,0 +1,17 @@
+package main
+
+const PI = 3.14
+const GAVITY = 9.81
+
+func main() {
+	const DAYS int = 7
+
+	const (
+		monday   = 1
+		tuesday  = 2
+		wenesday = 3
+		thursday = 4
+	)
+
+	// name := "A"
+}

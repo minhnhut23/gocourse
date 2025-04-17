ebad47322f3f114030e93043611c1bbdc7ff45f2
minhnhut23 <minhnhut23.work@gmail.com>
2025-04-17 11:38:58 +0700

    Exit

---
+++ basics/exit.go
@@ -0,0 +1,19 @@
+package basics
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	defer fmt.Println("Defer statement")
+
+	fmt.Println("Starting main function")
+	
+	// Exit with status code of 1
+	os.Exit(1)
+
+	// This will never be executed
+	fmt.Println("End of main function")
+}

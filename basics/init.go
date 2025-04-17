4933336aaefeb0df9c13bf3812fdf3fdbf4e4ce4
minhnhut23 <minhnhut23.work@gmail.com>
2025-04-17 11:56:05 +0700

    Init

---
+++ basics/init.go
@@ -0,0 +1,21 @@
+package basics
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initializing package 1...")
+}
+
+func init() {
+	fmt.Println("Initializing package 2...")
+}
+
+func init() {
+	fmt.Println("Initializing package 3...")
+}
+
+func main() {
+
+	fmt.Println("Inside the main function")
+
+}

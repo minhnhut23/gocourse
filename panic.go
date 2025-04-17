50576f8e344c3d9748c4a5fd4a5885b3cc7d800e
minhnhut23 <minhnhut23.work@gmail.com>
2025-04-17 10:50:06 +0700

    Panic

---
+++ panic.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// panic(interface())
+
+	// Example of a valid input
+	process(10)
+
+	// Example of an invalid input
+	process(-10)
+
+}
+
+func process(input int) {
+	defer fmt.Println("Defferd 1")
+	defer fmt.Println("Defferd 2")
+	
+	if input < 0 {
+		fmt.Println("Before panic")
+		panic("Input must be non-negative number")
+	}
+	fmt.Println("Processing input:", input)
+}

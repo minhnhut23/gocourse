a8c98bfbe17fdcfa02d976bf25d8b81cbb08a324
minhnhut23 <minhnhut23.work@gmail.com>
2025-04-23 15:04:37 +0700

    Methods

---
+++ intermediate/methods.go
@@ -0,0 +1,50 @@
+package intermediate
+
+import "fmt"
+
+type Shape struct {
+	Rectangle
+}
+
+type Rectangle struct {
+	length float64
+	width  float64
+}
+
+// Method with value receiver
+func (r Rectangle) Area() float64 {
+	return r.length * r.width
+}
+
+// Method with pointer receiver
+func (r *Rectangle) Scale(factor float64) {
+	r.length *= factor
+	r.width *= factor
+}
+
+func main() {
+
+	rect := Rectangle{length: 10, width: 4}
+	area := rect.Area()
+	fmt.Println(area)
+	rect.Scale(2)
+	fmt.Println(rect.Area())
+
+	num := MyInt(-5)
+	num1 := MyInt(5)
+	fmt.Println(num.IsPositive(), num1.IsPositive())
+	fmt.Println(num.WelcomeMessage())
+
+	s := Shape{Rectangle: Rectangle{length: 10, width: 4}}
+	fmt.Println(s.Area())
+}
+
+type MyInt int
+
+func (m MyInt) IsPositive() bool {
+	return m > 0
+}
+
+func (MyInt) WelcomeMessage() string {
+	return "Welcome to MyInt Type"
+}
